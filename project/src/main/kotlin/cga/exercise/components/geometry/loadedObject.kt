af59edc0ccf24eb7c589ddba7d2f3309fccb4c07
Mathieu Sorg <sorgmathieu@googlemail.com>
2020-07-16 13:57:59 +0200

    Update loadedObject.kt

--- project/src/main/kotlin/cga/exercise/components/geometry/loadedObject.kt
+++ project/src/main/kotlin/cga/exercise/components/geometry/loadedObject.kt
@@ -3,10 +3,16 @@ package cga.exercise.components.geometry
 import cga.exercise.components.texture.Texture2D
 import cga.framework.OBJLoader
 import org.joml.Vector2f
+import org.joml.Vector4i
 import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL30
 
-class loadedObject(objectPath : String, diffTexturPath: String, emitTexturPath: String, specTexturPath: String) {
+class loadedObject(
+        objectPath : String, diffTexturPath: String, emitTexturPath: String, specTexturPath: String,
+        texturMipmap : Boolean = true,
+        diffTexParams : Vector4i = Vector4i(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR),
+        emitTexParams : Vector4i = Vector4i(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR),
+        specTexParams : Vector4i = Vector4i(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR)) {
 
     var meshData : MutableList<OBJLoader.OBJMesh>
     var createdMeshes : MutableList<Mesh>
@@ -21,12 +27,12 @@ class loadedObject(objectPath : String, diffTexturPath: String, emitTexturPath: 
         val loadedData = OBJLoader.loadOBJ(objectPath)
         meshData = loadedData.objects[0].meshes
         // Erstellen der Texturen
-        diffTextur = Texture2D(diffTexturPath, true)
-        diffTextur.setTexParams(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR)
-        emitTextur = Texture2D(emitTexturPath, true)
-        emitTextur.setTexParams(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR)
-        specTextur = Texture2D(specTexturPath, true)
-        specTextur.setTexParams(GL30.GL_REPEAT, GL30.GL_REPEAT, GL30.GL_LINEAR_MIPMAP_LINEAR, GL30.GL_LINEAR)
+        diffTextur = Texture2D(diffTexturPath, texturMipmap)
+        diffTextur.setTexParams(diffTexParams[0], diffTexParams[2], diffTexParams[3], diffTexParams[4])
+        emitTextur = Texture2D(emitTexturPath, texturMipmap)
+        emitTextur.setTexParams(emitTexParams[0], emitTexParams[2], emitTexParams[3], emitTexParams[4])
+        specTextur = Texture2D(specTexturPath, texturMipmap)
+        specTextur.setTexParams(specTexParams[0], specTexParams[2], specTexParams[3], specTexParams[4])
         // Erstellen des Materials mit den erstellten Texturen
         objectMaterial = Material(diffTextur, emitTextur, specTextur, 60f, Vector2f(64.0f, 64.0f))
         // Erstellen der Meshes
