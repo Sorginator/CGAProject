cee46854522b6dda6c875136eca3c1ad2f4c5e22
Mathieu Sorg <sorgmathieu@googlemail.com>
2020-07-24 15:59:02 +0200

    Create Walkable.kt

---
+++ project/src/main/kotlin/cga/exercise/components/object/Walkable.kt
@@ -0,0 +1,8 @@
+package cga.exercise.components.`object`
+
+import cga.exercise.components.geometry.Mesh
+import cga.exercise.components.geometry.Renderable
+
+class Walkable(Meshes: MutableList<Mesh>) : Renderable(Meshes) {
+
+}
