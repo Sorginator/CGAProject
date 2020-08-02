2a0fc3388e3a182130d3caee21553c484bea9623
Mathieu Sorg <sorgmathieu@googlemail.com>
2020-08-02 10:42:33 +0200

    Update Walkable.kt
    vererbung mit textured Object entfernt

--- project/src/main/kotlin/cga/exercise/components/object/Walkable.kt
+++ project/src/main/kotlin/cga/exercise/components/object/Walkable.kt
@@ -4,12 +4,14 @@ import cga.exercise.components.geometry.Mesh
 import cga.exercise.components.geometry.Renderable
 import cga.exercise.components.shader.ShaderProgram
 import cga.framework.GameWindow
+import cga.framework.ModelLoader
 import org.joml.Math
 import org.joml.Vector3f
 import org.lwjgl.glfw.GLFW
 
-open class Walkable(path: String, posX: Float, posY: Float, posZ: Float, rotX: Float= 0f, rotY: Float= 0f, rotZ: Float= 0f, scaleX: Float= 1f, scaleY: Float= 1f, scaleZ: Float= 1f, horizontalReverse: Boolean= false, verticalReverse: Boolean= false) : texturedObject(path, posX, posY, posZ, rotX, rotY, rotZ, scaleX, scaleY, scaleZ) {
+open class Walkable(path: String, posX: Float, posY: Float, posZ: Float, rotX: Float= 0f, rotY: Float= 0f, rotZ: Float= 0f, scaleX: Float= 1f, scaleY: Float= 1f, scaleZ: Float= 1f, horizontalReverse: Boolean= false, verticalReverse: Boolean= false) {
 
+    val loadedObject: Renderable
     var hReverse: Int = 1
     var vReverse: Int = 1
     var speedForward: Float = 5f
@@ -18,13 +20,19 @@ open class Walkable(path: String, posX: Float, posY: Float, posZ: Float, rotX: F
     var speedPush: Float = 1f
     var isMoving: Boolean = false
     var bleibtAufDemBoden: Boolean = true
-    var defaultHeight: Float = loadedObject.getPosition().y
+    var defaultHeight: Float
 
     init {
-        if (horizontalReverse)
+        loadedObject = ModelLoader.loadModel(path,  Math.toRadians(rotX), Math.toRadians(rotY), Math.toRadians(rotZ))?: throw IllegalArgumentException("Could not load the model")
+        loadedObject.scaleLocal(Vector3f(scaleX, scaleY, scaleZ))
+        loadedObject.translateGlobal(Vector3f(posX, posY, posZ))
+        if (horizontalReverse) {
             hReverse = -1
-        if (verticalReverse)
+        }
+        if (verticalReverse) {
             vReverse = -1
+        }
+        defaultHeight = loadedObject.getPosition().y
     }
 
     open fun walk(timeDifference: Float, window: GameWindow, time: Float){
@@ -58,13 +66,9 @@ open class Walkable(path: String, posX: Float, posY: Float, posZ: Float, rotX: F
         if (window.getKeyState(GLFW.GLFW_KEY_D)) {
             loadedObject.rotateAroundPoint(0f, Math.toRadians(-1f*timeDifference*vReverse*speedRotate),0f,loadedObject.getPosition())
         }
-        /*
-        print("Position: ")
-        print(loadedObject.getPosition().x)
-        print(", ")
-        print(loadedObject.getPosition().y)
-        print(", ")
-        println(loadedObject.getPosition().z)
-        */
+    }
+
+    open fun render(shader: ShaderProgram){
+        loadedObject.render(shader)
     }
 }
